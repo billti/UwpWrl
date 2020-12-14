c451b4b614ae6dff21c279c97cabaee82df9b501
Bill Ticehurst <billti@microsoft.com>
2020-12-14 08:47:06 -0800

    Added precompiled header

--- main.cpp
+++ main.cpp
@@ -1,33 +1,4 @@
-#if !defined(WINAPI_FAMILY)
-#define WINAPI_FAMILY WINAPI_FAMILY_PC_APP
-#endif
-
-#if !defined(UNICODE) || !defined(_UNICODE)
-#error "Compile with Unicode"
-#endif
-
-#if !defined(NDEBUG) && !defined(_DEBUG)
-#error "Define either NDEBUG for Release or _DEBUG for Debug builds"
-#endif
-
-#if !defined(__cplusplus) || \
-    !defined(_MSC_VER) && __cplusplus < 201703L || \
-    defined(_MSC_VER) && _MSVC_LANG < 201703L
-#error Compile with C++17 or later
-#endif
-
-#if defined(__cplusplus_winrt)
-#error "Don't compiled with the /ZW switch"
-#endif
-
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <Windows.h>
-
-#include <windows.foundation.h>
-
-#include <wrl.h>
-#include <wrl\wrappers\corewrappers.h>
+#include "pch.h"
 
 using Microsoft::WRL::ComPtr;
 using Microsoft::WRL::Wrappers::HString;
